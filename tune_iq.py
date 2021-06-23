7857eb06e5ef8eb79ed272b934a5408ab0e89aff
adibacco <a.dibacco@gmail.com>
2021-06-23 13:34:57 +0000

    tune

--- tune_iq.py
+++ tune_iq.py
@@ -5,21 +5,37 @@ import numpy as np
 from math import pi as pi
 from scipy import signal as sig
 
+MAX_SIG = 30000
 
 inputfile = 'ssb_30k_50M_0806-16384-.bin'
 inputfile = sys.argv[1]
-off_i = float(sys.argv[2])
-off_q = float(sys.argv[3])
+save_file = 1
+
+try:
+    off_i = float(sys.argv[2])
+except IndexError:
+    off_i = 0
+    save_file = 0
+
+try:
+    off_q = float(sys.argv[3])
+except IndexError:
+    off_q = 0
+    save_file = 0
+
+
 
 try:
     amp_i = float(sys.argv[4])
 except IndexError:
     amp_i = 1.0
+    save_file = 0
 
 try:
     amp_q = float(sys.argv[5])
 except IndexError:
     amp_q = 1.0
+    save_file = 0
 
 
 
@@ -41,11 +57,27 @@ print('max Q ' + str(max_q) + ' min Q ' + str(min_q) + ' mean Q ' + str(mean_q))
 
 l = len(iq)
 
+
+
 io = i*amp_i 
 qo = q*amp_q 
+
+
+
+
 io = io + off_i
 qo = qo + off_q
 
+max_io = np.max(io);
+min_io = np.min(io);
+max_qo = np.max(qo);
+min_qo = np.min(qo);
+
+
+
+
+if (save_file == 0):
+    exit()
 
 print('Output signal')
 max_i = np.max(io)
